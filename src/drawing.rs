7c69358e689549efc43d55c7dd9e2edaad77ad0e
edpenington <edpenington@gmail.com>
2016-06-19 22:20:18 +0100

    Created player view struct that can be adjusted

--- src/drawing.rs
+++ src/drawing.rs
@@ -7,7 +7,63 @@ pub struct Vertex {
     pub color    : [f32; 3],
 }
 
-fn view_matrix(
+struct View {
+    position: [f32;3],
+    direction: [f32;3],
+    up: [f32; 3],
+}
+
+impl View {
+    pub fn vector_length(vec3: &[f32;3]) -> f32 {
+        let sum_sqr_vec = (vec3[0] * vec3[0]) +
+            (vec3[1] * vec3[1]) +
+            (vec3[2] * vec3[2]);
+        sum_sqr_vec.sqrt()
+    }
+
+    pub fn change_position(&mut self, dx: f32, dy: f32, dz: f32)
+    {
+        self.position = [
+            self.position[0] + dx,
+            self.position[1] + dy,
+            self.position[2] + dz
+        ];
+    }
+
+    pub fn as_matrix(&self) ->
+        [[f32;4];4] {
+            let f = {
+                let f = self.direction;
+                let len = View::vector_length(&f);
+                [f[0] / len, f[1] / len, f[2] / len]
+            };
+            let s = [self.up[1] * f[2] - self.up[2] * f[1],
+                     self.up[2] * f[0] - self.up[0] * f[2],
+                     self.up[0] * f[1] - self.up[1] * f[0]];
+            let s_norm = {
+                let len = View::vector_length(&s);
+                [s[0] / len, s[1] / len, s[2] / len]
+            };
+
+            let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+                     f[2] * s_norm[0] - f[0] * s_norm[2],
+                     f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+            let position = self.position;
+            let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+                     -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+                     -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+            [
+                [s[0], u[0], f[0], 0.0],
+                [s[1], u[1], f[1], 0.0],
+                [s[2], u[2], f[2], 0.0],
+                [p[0], p[1], p[2], 1.0],
+            ]
+        }
+}
+
+/*fn view_matrix(
     position: &[f32;3],
     direction: &[f32;3],
     up: &[f32;3])
@@ -41,7 +97,7 @@ fn view_matrix(
                      [s[2], u[2], f[2], 0.0],
                      [p[0], p[1], p[2], 1.0],
                  ]
-    }
+    }*/
 
 
 pub fn draw_rotating_cube() {
@@ -137,6 +193,12 @@ pub fn draw_rotating_cube() {
     let mut t: f32 = -0.5;
     let mut r: f32 = 0.0;
 
+    let mut player_view = View {
+        position: [2.0, -1.0, 1.0],
+        direction:[-2.0, 1.0, 1.0],
+        up: [0.0, 1.0, 0.0]
+        };
+
     loop {
 
         //Update time-step t
@@ -166,9 +228,8 @@ pub fn draw_rotating_cube() {
             },
             .. Default::default()
         };
-        let view = view_matrix(&[2.0, -1.0, 1.0*t],
-             &[-2.0, 1.0, 1.0],
-             &[0.0, 1.0, 0.0]);
+        player_view.change_position(0.0, 0.0, -t);
+        let view = player_view.as_matrix();
         let perspective = {
             let (width, height) = target.get_dimensions();
             let aspect_ratio = height as f32 / width as f32;
